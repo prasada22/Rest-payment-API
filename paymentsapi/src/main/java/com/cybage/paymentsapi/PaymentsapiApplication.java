0fa3a96c22dfb181251bb369a81f79bf13167a2d
Prasad <prasada@cybage.com>
2023-01-12 12:04:00 +0530

    folder structure

---
+++ paymentsapi/src/main/java/com/cybage/paymentsapi/PaymentsapiApplication.java
@@ -0,0 +1,13 @@
+package com.cybage.paymentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsapiApplication.class, args);
+	}
+
+}
